7a9580becfc333cb81d55d7d56a68bcea855f1a4
WeiChen-Lin <asdfg09487@gmail.com>
2021-08-01 17:36:44 +0800

    修改藉由email連結註冊的js

--- static/js/mail.js
+++ static/js/mail.js
@@ -1,6 +1,6 @@
 let password_check = false;
-let password_site = document.querySelector("body > div.signbox > div.signUp > form > div:nth-child(4) > input[type=password]");
-
+const password_site = document.querySelector("body > div.signbox > div > form > div:nth-child(4) > input[type=password]");
+const username_site = document.querySelector("body > div.signbox > div > form > div:nth-child(3) > input[type=text]");
 /* 密碼處做檢查 */
 password_site.addEventListener("change", ()=>{
     let pwd_re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
@@ -43,35 +43,58 @@ function CheckSignUp(){
 function postToAPI(){
     let data = getform(document.querySelector("body > div.signbox > div.signUp > form"));
     data.loginRoute = 1;
-    data.userEmail = document.querySelector("body > div.signbox > div > form > div.email").textContent;
-    data.teamID = teamID;
     let data_to_python = JSON.stringify(data);
-    let requestUrl = domain_name + "/api/mail"
+    let requestUrl = domain_name + "/api/sign"
     let request = new XMLHttpRequest();
+    signUpbut.textContent = "";
+    loaderIcon(signUpbut, 48);
     request.onload = function(){
         if(request.status == 200){
             window.location.href = domain_name + "/home"
         } else {
+            /* 清除錯誤訊息 */
+            clearSignError();
+            
+            /* loading動畫關閉 */
+            signUpbut.textContent = "Sign Up";
+            closeIcon(signUpbut);
+            
+
             let json = JSON.parse(request.responseText);
-            console.log(json);
             if(json.error == 1){
                 username_site.style.border = "1px red solid";
-            } else if(json.error == 2){
-                email_site.style.border = "1px red solid";
-                email_check = false;
-                CheckSignUp();
-            } else if(json.error ==3){
-                email_site.style.border = "1px red solid";
-                email_check = false;
-                CheckSignUp();
+                drawSignError(username_site, "Repeat username")
+
             } else if(json.error == 4){
-                password_site.style.border = "2px red solid";
+
+                password_site.style.borderBottom = "2px red solid";
                 password_check = false;
+                drawSignError(email_site, "Invalid password format")
                 CheckSignUp();
             }
         }
     }
-    request.open("PATCH", requestUrl, true);
+    request.open("POST", requestUrl, true);
     request.setRequestHeader('content-type', 'application/json');
     request.send(data_to_python);
+}
+
+/* 繪出錯誤訊息 */ 
+function drawSignError(site, error){
+    let p = document.createElement("p");
+    p.className = "error_message";
+    p.style.color = "red";
+    let p_error = document.createTextNode(error);
+    p.appendChild(p_error);
+    site.parentNode.appendChild(p);
+}
+
+/* 清除錯誤訊息 */
+function clearSignError(){
+    let error = document.getElementsByClassName("error_message");
+    if(error){
+        for(let i = 0; i < error.length; i++){
+            error[i].parentNode.removeChild(error[i]);
+        }
+    }
 }
