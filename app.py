ed517cb4a99018bc7b6f1e9e1985fb5f0831dd9f
WeiChen-Lin <asdfg09487@gmail.com>
2021-08-01 15:48:48 +0800

    decode token change

--- app.py
+++ app.py
@@ -478,10 +478,8 @@ def mailsend():
 
             token  = jwt.encode({"userEmail": email, "teamID": teamID, "status":0}, config["jwt_secret_key"])
 
-        
-        token_on_url = json.loads(token)
  
-        url = "https://doitouob.com/api/mail?token="+ token_on_url
+        url = "https://doitouob.com/api/mail?token="+ token.decode("utf-8")
         msg_title = 'Sincerely invite you to join the team'
         msg_sender = ("Do it!", "DoitSender2021@gmail.com")
         msg_recipients = []
